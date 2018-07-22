dfd088f231cb3bd2357f763d22fb9a8d01f2c2c5
Mike Eling <mike.eling97@gmail.com>
2018-07-22 03:44:54 +0200

    🛠 Fixed no translation exception

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -9,7 +9,10 @@ export default class Langlate extends Component {
    * @returns {string}
    */
   static raw(text, language) {
-    if(language === undefined || language === null) { if(text !== undefined) language = Object.keys(text)[0]; else language = "en_US"; }
+    if (language === undefined || language === null) {
+      if (text !== undefined) language = Object.keys(text)[0];
+      else language = "en_US";
+    }
     return translate(language, text);
   }
 
@@ -29,12 +32,14 @@ export default class Langlate extends Component {
  * @returns {string}
  */
 function translate(language, text) {
-  if (text === undefined || !text instanceof Object) throw Error("The text property is not an object.");
+  if (text === undefined || !text instanceof Object)
+    throw Error("The text property is not an object.");
 
   if (Object.keys(text).length > 0) {
     if (language === null) language = Object.keys(text)[0];
     for (let i = 0; i < Object.keys(text).length; i++) {
       const key = Object.keys(text)[i];
+      if (typeof key != "string") key = Object.keys(text)[0];
       if (!language) language = key;
       if (key === language) {
         return text[key];
